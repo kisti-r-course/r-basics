b78102c82cfa5be672517c730dc9f6ad3f42dd0b
박서윤 (Riley Park) <104350185+riley-seoyoon@users.noreply.github.com>
2023-07-12 21:28:33 +0900

    Day 2 files

---
+++ day_2/mini_project_2.R
@@ -0,0 +1,66 @@
+# Mini Project: Salaries Dashboard
+
+## Step 1: Data Preparation
+
+# Load the salaries dataset
+salaries <- read.csv("./data/Salaries.csv", header = TRUE)
+
+
+
+
+## Step 2: Dashboard Setup
+
+# Load required libraries
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+# Define UI for the salaries dashboard
+ui <- fluidPage(
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+)
+
+## Step 3: Server Logic
+
+# Define server logic for the salaries dashboard
+server <- function(input, output) {
+  
+  # Create a reactive subset of data based on user input
+
+  
+  
+  
+  
+  
+  
+  # Generate summary statistics based on filtered data
+
+  
+  
+  
+  # Generate histogram plot based on filtered data
+
+  
+  
+  
+  
+}
+
+## Step 4: Run the Dashboard
+
+# Run the Shiny application
+
