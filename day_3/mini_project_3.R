7591874619546685375231a06923a9feec3c326d
박서윤 <104350185+riley-seoyoon@users.noreply.github.com>
2023-11-24 12:54:37 +0900

    Update mini_project_3.R

--- day_3/mini_project_3.R
+++ day_3/mini_project_3.R
@@ -5,94 +5,88 @@ library(shiny)
 library(caret)
 
 # Step 1: Read the CSV file and view the dataset
-
-
+titanic <- read.csv("./data/train.csv", stringsAsFactors = TRUE)
+head(titanic)
 
 # Step 2: Preprocessing with dplyr
 # Select relevant columns and handle missing values
-
-
-
-
+titanic_processed <- titanic %>%
 
 # Step 3: Exploratory data analysis with ggplot2
 # Visualize survival rate by passenger class using a loop
-
-
-
-
-
-
-
+for (feature in c()) {
+  ggplot()
+}
 
 # Step 4: Create a basic R Shiny dashboard with selected features
 # Define UI
+ui <- fluidPage(
+  titlePanel("Titanic Passenger Analysis"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
 
-
-
-
-
-
-
-
-
-
+        
+        
+      )
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
 
 # Define server
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    # Generate dynamic plot based on selected features using if and for loops
+    if ("Pclass" %in% input$features) {
+      p <- ggplot() +
+        
+      print(p)
+    }
+    
+    if ("Sex" %in% input$features) {      
+      p <- ggplot() +
+      
+      print(p)
+    }
+    
+    if ("Age" %in% input$features) {
+      p <- ggplot() +
+        
+      print(p)
+    }
+    
+    if ("Fare" %in% input$features) {
+      p <- ggplot() +
+        
+      print(p)
+    }
+  })
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+shinyApp(ui = ui, server = server)
 
 # Step 5: Machine learning with logistic regression
-
+model <- glm()
 
 # Step 6: Evaluate the model
-
-
-
+predictions <- predict(    , newdata =    , type = "response")
+predictions <- ifelse(predictions > 0.5, "1", "0")
 
 # Ensure both predictions and actual values are factors with the same levels
-
-
+predictions <- factor()
+titanic_processed$Survived <- factor()
 
 # Calculate accuracy
-
+accuracy <- mean(predictions == titanic_processed$Survived)
 
 # Calculate precision, recall, and F1-score
-
-
-
-
-
-
-
+confusion <- confusionMatrix(predictions, titanic_processed$Survived)
+precision <- confusion$byClass["Pos Pred Value"]
+recall <- confusion$byClass["Sensitivity"]
+f1_score <- confusion$byClass["F1"]
 
 # Print the evaluation metrics
 print("Evaluation Metrics:")
@@ -100,4 +94,3 @@ print(paste("Accuracy:", accuracy))
 print(paste("Precision:", precision))
 print(paste("Recall:", recall))
 print(paste("F1-Score:", f1_score))
-
